648832074c01c901e2e1ed414a39add7c67610a3
shacharbard <shachar.bard@gmail.com>
2024-06-06 15:14:37 +0300

    fix background color

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import ChatComponent from "./ChatComponents";
 
 export default function Home() {
   return (
-    <div class="container max-width mx-auto bg-gradient-to-r from-indigo-600 to-indigo-200">
+    <div>
       
       <ChatComponent />
       
